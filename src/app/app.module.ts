1c531476cc814e69b9f5fd65db8d066205be3559
Nipuni Premadasa <nipunipremadasa923@gmail.com>
2023-05-20 00:39:19 +0530

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { DoughnutChartComponent } from './charts/doughnut-chart/doughnut-chart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent,
+    DoughnutChartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
